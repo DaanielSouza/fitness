d48a4db2a12b2bef7f531db82c496be1110692bb
Daniel Souza avelino <danielavelino@ucl.br>
2022-10-29 23:14:32 -0300

    Header + Carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
 import { FooterComponent } from './footer/footer.component';
+import { CarouselComponent } from './main/carousel/carousel.component';
+import { ExercisesComponent } from './main/exercises/exercises.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MainComponent,
-    FooterComponent
+    FooterComponent,
+    CarouselComponent,
+    ExercisesComponent
   ],
   imports: [
     BrowserModule,
